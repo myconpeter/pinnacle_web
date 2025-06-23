80a9e85fbc19388b3895a3c9966b98206e02c770
Micheal Peter <79177019+myconpeter@users.noreply.github.com>
2025-06-23 10:48:28 -0700

    Update ContactUs.tsx

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -5,7 +5,7 @@ const ContactUs = () => {
 	return (
 		<div className='relative w-full px-3 md:px-20 mt-10 md:mt-24 items-center md:pb-48'>
 			<a
-				href='https://wa.me/2348029837117?text=Hello%20Pinnacle%20Education%20and%20Training%20Limited.%2C%20I%20am%20interested%20in '
+				href='https://wa.me/2348029837117?text=Hello%20Pinnacle%20Education%20and%20Training%20Limited%2C%20I%20am%20interested%20in '
 				target='_blank'
 				rel='noopener noreferrer'>
 				<button className='md:p-3 p-2 bg-light-tomato text-sm md:text-xl rounded-3xl text-white'>
@@ -34,7 +34,7 @@ const ContactUs = () => {
 			<div className='flex mt-2 md:mt-5 items-center gap-2'>
 				<IoLogoWhatsapp className='text-green-600 text-xl' />
 				<a
-					href='https://wa.me/2348029837117?text=Hello%20Pinnacle%20Education%20and%20Training%20Limited.%2C%20I%20am%20interested%20in '
+					href='https://wa.me/2348029837117?text=Hello%20Pinnacle%20Education%20and%20Training%20Limited%2C%20I%20am%20interested%20in '
 					target='_blank'
 					rel='noopener noreferrer'
 					className='text-black md:text-xl'>
